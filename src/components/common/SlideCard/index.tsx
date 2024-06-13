70059930b6e9e32b989db6ce268eeb3a488fba68
ovitormacel <ovitormacel00@gmail.com>
2024-06-13 10:08:20 -0300

    Finalização do Design do Slider de Eventos em Destaque

--- src/components/common/SlideCard/index.tsx
+++ src/components/common/SlideCard/index.tsx
@@ -1,13 +1,25 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+
 interface SlideProps {
     image: string,
     title: string
 }
 
+
 const SlideCard = ({image, title}: SlideProps) => {
     return (
         <>
-            <div>
-                <img src={image} alt={title} />
+            <div className={styles.slideContent}>
+                <div className={styles.slideImage} style={{backgroundImage: `url(${image})`}}></div>
+
+                <div className="container">
+                    <div className={styles.slideInfo}>
+                        <p className={styles.date}>11/07<span>Sexta</span></p>
+                        <h2 className={styles.eventTitle}>{title}</h2>
+                        <Link href="#" className={styles.slideLink}>Ver Mais</Link>
+                    </div>
+                </div>
             </div>
         </>
     )
