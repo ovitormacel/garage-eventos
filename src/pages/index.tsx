bba84b79faf28dc58719e2bc74841897c7c9e31d
ovitormacel <ovitormacel00@gmail.com>
2024-09-15 14:36:44 -0300

    BUGFIX com alterações na função getStaticProps em index.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,105 @@ interface props {
   events: Event[]
 }
 
-const Home: NextPage = ({events}: props) => {
+const Home: NextPage = () => {
+
+  let events = [
+    {
+        id: 1,
+        name: "Brothers Rush",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        date: {
+            day: 17,
+            month: 8,
+            year: 2024,
+            hour: 21,
+            minutes: 0,
+        },
+        classification: 18,
+        ticketPrice: 119,
+        cover: "brothersRush_back"
+    },
+
+    {
+        id: 2,
+        name: "Assalariados",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        date: {
+            day: 19,
+            month: 8,
+            year: 2024,
+            hour: 21,
+            minutes: 0,
+        },
+        classification: 18,
+        ticketPrice: 119,
+        cover: "assalariados_back"
+    },
+
+    {
+        id: 3,
+        name: "Colab Santos",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        date: {
+            day: 30,
+            month: 8,
+            year: 2024,
+            hour: 21,
+            minutes: 0,
+        },
+        classification: 18,
+        ticketPrice: 119,
+        cover: "colabSantos_back"
+    },
+
+    {
+        id: 4,
+        name: "Ana Luiza",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        date: {
+            day: 10,
+            month: 9,
+            year: 2024,
+            hour: 21,
+            minutes: 0,
+        },
+        classification: 18,
+        ticketPrice: 119,
+        cover: "analuiza_back"
+    },
+
+    {
+        id: 5,
+        name: "CubaLibre",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        date: {
+            day: 20,
+            month: 6,
+            year: 2024,
+            hour: 21,
+            minutes: 0,
+        },
+        classification: 18,
+        ticketPrice: 119,
+        cover: "cubaLibre_back"
+    },
+
+    {
+        id: 6,
+        name: "Engenheiros de Dubai",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        date: {
+            day: 20,
+            month: 9,
+            year: 2024,
+            hour: 21,
+            minutes: 0,
+        },
+        classification: 18,
+        ticketPrice: 119,
+        cover: "engenheirosDeDubai_back"
+    }
+];
 
   return (
     <>
@@ -39,21 +137,21 @@ const Home: NextPage = ({events}: props) => {
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  let events;
+// export const getStaticProps: GetStaticProps = async () => {
+//   let events;
 
-  try {
-    events = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/getAllNextEvents").then(res => res.json());
-  } catch (error) {
-    events = {}
-    throw new Error("Nenhum dado recuperado")
-  }
+//   try {
+//     events = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/getAllNextEvents").then(res => res.json());
+//   } catch (error) {
+//     events = {}
+//     throw new Error("Nenhum dado recuperado")
+//   }
 
-  return {
-    props: {
-      events
-    }
-  }
-}
+//   return {
+//     props: {
+//       events
+//     }
+//   }
+// }
 
 export default Home;
